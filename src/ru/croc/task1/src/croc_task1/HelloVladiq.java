6c3df7ddccce5e524c971d1f8092a43cdd62d894
VladiqDanilov <90576351+VladiqDanilov@users.noreply.github.com>
2022-10-23 19:55:38 +0300

    Add files via upload
    did the first task

---
+++ src/ru/croc/task1/src/croc_task1/HelloVladiq.java
@@ -0,0 +1,7 @@
+package croc_task1;
+
+public class HelloVladiq{
+	  public static void main(String[] args) {
+		    System.out.println("It's Vladiq write on [Java]");
+		  }
+		}
---
+++ src/ru/croc/task1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Vladiq
+ *
+ */
+module croc_task1 {
+}
