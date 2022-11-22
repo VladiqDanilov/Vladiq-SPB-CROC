511d9bbb2ee94514615ccf7fee2686be7a4ea565
VladiqDanilov <90576351+VladiqDanilov@users.noreply.github.com>
2022-11-22 15:21:15 +0300

    implemented regular expression support

--- src/ru/croc/task6/CommentRemover.java
+++ src/ru/croc/task6/CommentRemover.java
@@ -6,73 +6,29 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class CommentRemover {
-
-    public String removeJavaCommentsString(String fileCode) {
-    	String ithem = "";
-    	String[] sourceCodeStr = fileCode.split("\n");
-        ArrayList<String> result = new ArrayList<>();
-        StringBuilder strRes = new StringBuilder();
-        boolean texIndex = false;
-        
-        //string parsing
-
-        for (String curLine : sourceCodeStr) {
-            if (!texIndex) {
-                ithem = "";
-            }
-
-            for (int i = 0; i < curLine.length(); i++) {
-                if (texIndex) {
-                    if (curLine.charAt(i) == '*' && i + 1 < curLine.length() && curLine.charAt(i + 1) == '/') {
-                        texIndex = false;
-                        i++;
-                    }
-                } else {
-                    if (curLine.charAt(i) == '/' && i + 1 < curLine.length() && curLine.charAt(i + 1) == '/') {
-                        if ((i - 1 == -1 || curLine.charAt(i-1) != '"')) {
-                            break;
-                        }
-                    }
-
-                    if (curLine.charAt(i) == '/' && i + 1 < curLine.length() && curLine.charAt(i + 1) == '*') {
-                        if ((i - 1 == -1 || curLine.charAt(i-1) != '"')) {
-                            texIndex = true;
-                            i++;
-                            continue;
-                        }
-                    }
-                    ithem += curLine.charAt(i);
-                }
-            }
-            if (!texIndex) {
-                if (ithem.length() > 0) {
-                    result.add(ithem);
-                }
-            }
-        }
-        for (String i : result) {
-        	strRes.append(i).append("\n");
-        }
-        return strRes.toString();
+    public static String removeJavaCommentsStringRegex(String fileCode) {
+        return fileCode.replaceAll("//.*|/\\*(?s:.*?)\\*/", "");
+        //Удаление комментариев из строки с помощью регулярного выражения
     }
 
-    public String removeJavaCommentsFile(String fileName) throws IOException {
+    // Удаление комментариев из файла
+    public static String removeJavaCommentsFile(String fileName) throws IOException {
         String allText = null;
-        try (BufferedReader addReader = new BufferedReader(new FileReader(fileName))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             StringBuilder stringBuilder = new StringBuilder();
-            String curLine = addReader.readLine();
-            //delete comments from the source code
+            String curLine = bufferedReader.readLine();
+
             while (curLine != null) {
                 stringBuilder.append(curLine);
                 stringBuilder.append(System.lineSeparator());
-                curLine = addReader.readLine();
+                curLine = bufferedReader.readLine();
             }
             allText = stringBuilder.toString();
         } catch (IOException e) {
             e.printStackTrace();
         }
         assert allText != null;
-        return(removeJavaCommentsString(allText));
+        return(removeJavaCommentsStringRegex(allText));
     }
 }
 
--- src/ru/croc/task6/task6.java
+++ src/ru/croc/task6/task6.java
@@ -1,21 +1,38 @@
 package ru.croc.task6;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class task6 {
 	public static void main(String[] args) {
+		/*
+		System.out.print("Введите путь к обрабатываемому файлу:");
+		Scanner s = new Scanner(System.in);
+        String fimeName = s.nextLine();
+		try {
+            System.out.println(CommentRemover.removeJavaCommentsFile(fimeName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        */
         CommentRemover commentRemover = new CommentRemover();
-        String code = "/*\r\n"
-        		+ " * My first ever program in Java!\r\n"
-        		+ " */\r\n"
-        		+ "class Hello { // class body starts here \r\n"
-        		+ "  \r\n"
-        		+ "  /* main method */\r\n"
-        		+ "  public static void main(String[] args/* we put command line arguments here*/) {\r\n"
-        		+ "    // this line prints my first greeting to the screen\r\n"
-        		+ "    System.out.println(\"Hi!\"); // :)\r\n"
-        		+ "  }\r\n"
-        		+ "} // the end\r\n"
-        		+ "// to be continued...\r\n"
-        		+ "";
-        System.out.println(commentRemover.removeJavaCommentsString(code));
+        String codeSecond = "public class HelloVladiq{\r\n"
+        		+ "	  public static void main(String[] args) { //Запускаем возвращаемый метод\r\n"
+        		+ "	  /*\r\n"
+        		+ "	  Принтим некоторые значения,\r\n"
+        		+ "	  */\r\n"
+        		+ "	  //они будут видны в терминале\r\n"
+        		+ "		    System.out.println(\"It's Vladiq write on [Java]\");\r\n"
+        		+ "			/*\r\n"
+        		+ "			Владислав, с уважением\r\n"
+        		+ "			*/\r\n"
+        		+ "			//к вам */\r\n"
+        		+ "			/*\r\n"
+        		+ "			ко всем\r\n"
+        		+ "			*/\r\n"
+        		+ "			\r\n"
+        		+ "		  }\r\n"
+        		+ "		}";
+        System.out.println(CommentRemover.removeJavaCommentsStringRegex(codeSecond));
     }
 }
